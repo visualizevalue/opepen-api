a76ef3c74c88ae21143690fbd105dd89b0d6479d
jalil <jalil@1001.digital>
2023-05-02 14:29:38 +0200

    Remove reference creation

--- database/migrations/1683025921439_add_delegated_to_to_set_subscriptions_tables.ts
+++ database/migrations/1683025921439_add_delegated_to_to_set_subscriptions_tables.ts
@@ -5,14 +5,12 @@ export default class extends BaseSchema {
 
   public async up () {
     this.schema.alterTable(this.tableName, (table) => {
-      table.string('address').references('address').inTable('accounts').alter()
       table.string('delegated_by')
     })
   }
 
   public async down () {
     this.schema.alterTable(this.tableName, (table) => {
-      table.string('address').alter()
       table.dropColumn('delegated_by')
     })
   }
