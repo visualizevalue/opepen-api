8d31f3952fafffd931c280c853e10ea7020025c3
jalil <jalil@1001.digital>
2023-10-27 17:57:44 +0200

    Add publish helper command

---
+++ commands/PublishSetArtistData.ts
@@ -0,0 +1,37 @@
+import { BaseCommand, args } from '@adonisjs/core/build/standalone'
+import Account from 'App/Models/Account'
+import SetModel from 'App/Models/Set'
+import SetSubmission from 'App/Models/SetSubmission'
+import { DateTime } from 'luxon'
+
+export default class PublishSetArtistData extends BaseCommand {
+  /**
+   * Command name is used to run the command
+   */
+  public static commandName = 'set:publish-artist-data'
+
+  /**
+   * Command description is displayed in the "help" output
+   */
+  public static description = 'Create a set submission for for an existing set'
+
+  public static settings = {
+    loadApp: true,
+    stayAlive: false,
+  }
+
+  @args.string()
+  public submission: string
+
+  public async run() {
+    const submission = await SetSubmission.findByOrFail('uuid', this.submission)
+    const set = await SetModel.findOrFail(submission.setId)
+
+    set.creator = submission.creator
+    set.artistSignature = submission.artistSignature
+
+    await set.save()
+
+    this.logger.info(`Updated set: ${set.id}`)
+  }
+}
