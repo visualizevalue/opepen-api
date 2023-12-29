657cf767cb81a8694e505a19529068eae2e45442
jalil <jalil@1001.digital>
2023-12-29 18:02:04 +0100

    Update OpenSea

--- app/Services/OpenSea.ts
+++ app/Services/OpenSea.ts
@@ -1,24 +1,22 @@
+import api from 'api'
 import Env from '@ioc:Adonis/Core/Env'
 import Logger from '@ioc:Adonis/Core/Logger'
-import axios from 'axios'
+
+const sdk = api('@opensea/v2.0#acj27lqhfpwaj')
+sdk.auth(Env.get('OPENSEA_KEY'));
+sdk.server(Env.get('OPENSEA_BASE'));
 
 export default class OpenSea {
-  public static async updateMetadata(tokenId: string) {
+  public static async updateMetadata(identifier: string) {
     try {
-      const url = `${Env.get('OPENSEA_BASE')}/api/v2/chain/ethereum/contract/0x6339e5e072086621540d0362c4e3cea0d643e114/nfts/${tokenId}/refresh`
-      Logger.debug(`OpenSea URL: ${url}`)
-      await axios.post(
-        url,
-        {
-          headers: {
-            'accept': '*/*',
-            'x-api-key': Env.get('OPENSEA_KEY'),
-          }
-        }
-      )
+      sdk.refresh_nft({
+        chain: 'ethereum',
+        address: '0x6339e5e072086621540d0362c4e3cea0d643e114',
+        identifier,
+      })
     } catch (e) {
-      Logger.error(JSON.stringify(e, null, 4))
-      Logger.warn(`Force update on #${tokenId} failed!`)
+      Logger.error(JSON.stringify(e, null, 45))
+      Logger.warn(`Force update on #${identifier} failed!`)
     }
   }
 }
