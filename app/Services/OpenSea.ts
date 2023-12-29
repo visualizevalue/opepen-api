536133d4849b9fdb789e6226beabe01dc8cd00f8
jalil <jalil@1001.digital>
2023-12-29 17:47:25 +0100

    Debug OS Updates

--- app/Services/OpenSea.ts
+++ app/Services/OpenSea.ts
@@ -5,8 +5,10 @@ import axios from 'axios'
 export default class OpenSea {
   public static async updateMetadata(tokenId: string) {
     try {
+      const url = `${Env.get('OPENSEA_BASE')}/api/v2/chain/ethereum/contract/0x6339e5e072086621540d0362c4e3cea0d643e114/nfts/${tokenId}/refresh`
+      Logger.debug(`OpenSea URL: ${url}`)
       await axios.post(
-        `${Env.get('OPENSEA_BASE')}/api/v2/chain/ethereum/contract/0x6339e5e072086621540d0362c4e3cea0d643e114/nfts/${tokenId}/refresh`,
+        url,
         {
           headers: {
             'accept': '*/*',
@@ -15,6 +17,7 @@ export default class OpenSea {
         }
       )
     } catch (e) {
+      Logger.error(JSON.stringify(e, null, 4))
       Logger.warn(`Force update on #${tokenId} failed!`)
     }
   }
