d7079aece7a1a80dbcbbf4a0970fc0d21ae5bd2b
jalil <jalil@1001.digital>
2023-12-29 17:19:20 +0100

    Do a post request to update OS

--- app/Services/OpenSea.ts
+++ app/Services/OpenSea.ts
@@ -5,7 +5,7 @@ import axios from 'axios'
 export default class OpenSea {
   public static async updateMetadata(tokenId: string) {
     try {
-      await axios.get(
+      await axios.post(
         `${Env.get('OPENSEA_BASE')}/api/v2/chain/ethereum/contract/0x6339e5e072086621540d0362c4e3cea0d643e114/nfts/${tokenId}/refresh`,
         {
           headers: {
