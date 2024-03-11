38fda2be70ff26bd9b178bd88f0f18c961fd4099
jalil <jalil@1001.digital>
2024-03-11 16:42:20 +0100

    Fix set stats in migration

--- database/migrations/1709828859353_add_missing_data_points_to_sets_tables.ts
+++ database/migrations/1709828859353_add_missing_data_points_to_sets_tables.ts
@@ -12,6 +12,29 @@ export default class extends BaseSchema {
       table.jsonb('submission_stats')
       table.jsonb('submitted_opepen')
     })
+
+    this.defer(async db => {
+      const sets = await db.from('sets').whereNotNull('name')
+
+      for (const set of sets) {
+        const submission = await db.from('set_submissions').where('set_id', set.id).orderBy('published_at', 'desc').first()
+
+        if (! submission) continue
+
+        await db.from('set_submissions')
+          .where('id', submission.id)
+          .update({
+            submission_stats: set.submission_stats,
+            submitted_opepen: set.submitted_opepen,
+            reveal_strategy: set.reveal_strategy,
+            reveals_at: set.reveals_at,
+            notification_sent_at: set.notification_sent_at,
+            min_subscription_percentage: set.min_subscription_percentage,
+            reveal_block_number: set.reveal_block_number,
+            rounded_preview: set.rounded_preview,
+          })
+      }
+    })
   }
 
   public async down () {
