caaafc2be6379308d3a4470f8646d4dd8d9ba86a
jalil <jalil@1001.digital>
2024-04-03 22:11:50 +0100

    Fix sorting

--- app/Models/SetSubmission.ts
+++ app/Models/SetSubmission.ts
@@ -431,7 +431,9 @@ export default class SetSubmission extends BaseModel {
   })
 
   public static orderActiveByRemainingTime = scope((query: Builder) => {
-    query.orderByRaw(`COALESCE(reveals_at, '1990-01-01') asc, COALESCE(remaining_reveal_time, ${DEFAULT_REMAINING_REVEAL_TIME}) asc`)
+    query.orderByRaw(
+      `COALESCE(reveals_at, '3000-01-01') asc, COALESCE(remaining_reveal_time, ${DEFAULT_REMAINING_REVEAL_TIME}) asc`
+    )
   })
 
   public static orderByRemainingTime = scope((query: Builder) => {
