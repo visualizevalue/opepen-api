593d60f11ad0cd3d5ffc8d9841133ae39ace987b
jalil <jalil@1001.digital>
2024-05-12 15:52:44 +0200

    Clean up casts

--- app/Models/Cast.ts
+++ app/Models/Cast.ts
@@ -1,8 +1,5 @@
 import { DateTime } from 'luxon'
-import { BaseModel, BelongsTo, HasMany, ManyToMany, belongsTo, column, hasMany, manyToMany } from '@ioc:Adonis/Lucid/Orm'
-import Image from './Image'
-import SetSubmission from './SetSubmission'
-import Opepen from './Opepen'
+import { BaseModel, BelongsTo, belongsTo, column } from '@ioc:Adonis/Lucid/Orm'
 import Account from './Account'
 
 export default class Cast extends BaseModel {
@@ -42,66 +39,4 @@ export default class Cast extends BaseModel {
   })
   public account: BelongsTo<typeof Account>
 
-  @belongsTo(() => SetSubmission, {
-    foreignKey: 'submissionId',
-  })
-  public submission: BelongsTo<typeof SetSubmission>
-
-  @belongsTo(() => Opepen, {
-    foreignKey: 'opepenId',
-    localKey: 'tokenId',
-  })
-  public opepen: BelongsTo<typeof Opepen>
-
-  @belongsTo(() => Post, {
-    foreignKey: 'parentPostId',
-  })
-  public parent: BelongsTo<typeof Post>
-
-  @belongsTo(() => Image, {
-    foreignKey: 'imageId',
-  })
-  public image: BelongsTo<typeof Image>
-
-  @manyToMany(() => Image)
-  public images: ManyToMany<typeof Image>
-
-  @hasMany(() => Post, {
-    foreignKey: 'parentPostId',
-    onQuery: query => {
-      query.whereNull('deletedAt')
-      query.whereNotNull('approvedAt')
-    },
-  })
-  public commentsCount: HasMany<typeof Post>
-
-
-  @hasMany(() => Post, {
-    foreignKey: 'parentPostId',
-    onQuery: query => {
-      query.whereNull('deletedAt')
-      query.whereNotNull('approvedAt')
-      query.orderBy('createdAt')
-    },
-  })
-  public comments: HasMany<typeof Post>
-  @hasMany(() => Post, {
-    foreignKey: 'parentPostId',
-    onQuery: query => {
-      query.whereNull('deletedAt')
-      query.whereNotNull('approvedAt')
-      query.orderBy('createdAt', 'desc')
-      query.preload('account')
-      query.limit(3)
-    }
-  })
-  public latestComments: HasMany<typeof Post>
-
-  public serializeExtras() {
-    return {
-      commentsCount: this.$extras.commentsCount_count
-        ? parseInt(this.$extras.commentsCount_count)
-        : undefined,
-    }
-  }
 }
