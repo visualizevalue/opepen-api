3401210ea8e948e4886ef0fbf33c5ee4da4322be
jalil <jalil@1001.digital>
2024-07-12 09:35:13 +0200

    Ensure mails are sent only once

--- app/Controllers/Http/NotificationsController.ts
+++ app/Controllers/Http/NotificationsController.ts
@@ -8,7 +8,11 @@ export default class NotificationsController {
   public async general (_: HttpContextContract) {
     const users = await Account.query().withScopes(scopes => scopes.receivesEmail('General'))
 
+    const sentEmails = new Set()
+
     for (const user of users) {
+      if (sentEmails.has(user.email)) continue
+
       try {
         await new NotifyGeneralEmail(user).sendLater()
         Logger.info(`General email scheduled: ${user.email}`)
@@ -16,6 +20,8 @@ export default class NotificationsController {
         console.log(e)
         Logger.warn(`Error scheduling SetNotification email: ${user.email}`)
       }
+
+      sentEmails.add(user.email)
     }
   }
 
