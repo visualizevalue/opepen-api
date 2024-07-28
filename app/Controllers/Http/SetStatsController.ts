de4c3931880ef3cd6c2034430bf50f945ac3341d
jalil <jalil@1001.digital>
2024-07-28 18:22:03 -0500

    Add set edition floor stats

--- app/Controllers/Http/SetStatsController.ts
+++ app/Controllers/Http/SetStatsController.ts
@@ -1,4 +1,5 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { maxBigInt } from 'App/Helpers/bigints'
 import SetModel from 'App/Models/SetModel'
 
 export default class SetStatsController {
@@ -24,8 +25,34 @@ export default class SetStatsController {
       "40": 0,
     })
 
+    const groupedPriceDefaults: {
+      "1": null|bigint
+      "4": null|bigint
+      "5": null|bigint
+      "10": null|bigint
+      "20": null|bigint
+      "40": null|bigint
+    } = {
+      "1": null,
+      "4": null,
+      "5": null,
+      "10": null,
+      "20": null,
+      "40": null,
+    }
+    const listingPricesGroupedByEdition = set.opepen.reduce((acc, opepen) => {
+      if (opepen.price) {
+        acc[opepen.data.edition] = acc[opepen.data.edition] === null
+          ? opepen.price as bigint
+          : maxBigInt(opepen.price as bigint, acc[opepen.data.edition] as bigint)
+      }
+
+      return acc
+    }, groupedPriceDefaults)
+
     return {
-      totals: listingsGroupedByEdition
+      totals: listingsGroupedByEdition,
+      floors: listingPricesGroupedByEdition,
     }
   }
 
