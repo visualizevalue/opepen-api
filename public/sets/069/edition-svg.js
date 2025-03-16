09539ea9caa660805b4612aee41d49b0407c98dd
jalil <jalil@1001.digital>
2025-03-16 13:46:05 +0100

    Fix scaling on mobile

--- public/sets/069/edition-svg.js
+++ public/sets/069/edition-svg.js
@@ -6,42 +6,42 @@ export const editionSVG = ({
       width="25"  height="20"
       stroke="currentColor"
       fill="${ edition == 1 ? 'currentColor' : '' }"
-      stroke-width="5"
+      stroke-width="6"
     />
     <rect
       x="178" y="103"
       width="25"  height="80"
       stroke="currentColor"
       fill="${ edition == 4 ? 'currentColor' : '' }"
-      stroke-width="5"
+      stroke-width="6"
     />
     <rect
       x="153" y="103"
       width="25"  height="100"
       stroke="currentColor"
       fill="${ edition == 5 ? 'currentColor' : '' }"
-      stroke-width="5"
+      stroke-width="6"
     />
     <rect
       x="103" y="103"
       width="50"  height="100"
       stroke="currentColor"
       fill="${ edition == 10 ? 'currentColor' : '' }"
-      stroke-width="5"
+      stroke-width="6"
     />
     <rect
       x="3" y="103"
       width="100" height="100"
       stroke="currentColor"
       fill="${ edition == 20 ? 'currentColor' : '' }"
-      stroke-width="5"
+      stroke-width="6"
     />
     <rect
       x="3" y="3"
       width="200" height="100"
       stroke="currentColor"
       fill="${ edition == 40 ? 'currentColor' : '' }"
-      stroke-width="5"
+      stroke-width="6"
     />
   </svg>`
 
